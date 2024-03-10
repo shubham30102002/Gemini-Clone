f6cc581c7e3978fcbf8a1cd2080f188c0f272edf
shubham30102002 <shubhamrg30@gmail.com>
2024-03-10 14:00:34 +0530

    main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <>
       <Sidebar/>
+      <Main/>
     </>
   );
 }

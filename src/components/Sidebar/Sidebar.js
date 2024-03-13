df166648497ae6e7945c6ac766d2706c95069038
shubham30102002 <shubhamrg30@gmail.com>
2024-03-13 18:24:11 +0530

    responsiveness

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,50 +1,64 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets } from "../../assets/assets";
+import { Context } from '../../context/Context';
 
 const Sidebar = () => {
 
     const [extended, setExtended] = useState(false);
-
+    const { onSent, prevPrompts, setRecentPrompt , newChat} = useContext(Context);
+    
+    const loadPrompt = async(prompt) => {
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
+    
     return (
         <div className='sidebar'>
             <div className='top'>
-                <img src={assets.menu_icon} alt='' className='menu' onClick={() => setExtended(prev=> !prev)}/>
+                <img src={assets.menu_icon} alt='' className='menu' onClick={() => setExtended(prev => !prev)} />
 
-                <div className='new-chat'>
+                <div onClick={() => newChat()} className='new-chat'>
                     <img src={assets.plus_icon} alt='' />
                     {extended ? (<p>New Chat</p>) : null}
                 </div>
 
 
-                {extended ? (<div className='recent'>
-                    <p className='recent-title' >Recent</p>
-
-                    <div className='recent-entry'>
-                        <img src={assets.message_icon} alt='' />
-                        <p>What is react...</p>
+                {extended ? (
+                    <div className='recent'>
+                        <p className='recent-title'>Recent</p>
+                        {/* sidebar recent prompts  */}
+                        {/* error  */}
+                        {prevPrompts.map((item, index) => {
+                            return (
+                                <div onClick={() => loadPrompt(item)} className='recent-entry'>
+                                    <img src={assets.message_icon} alt='' />
+                                    <p>{item.slice(0, 18)}...</p>
+                                </div>
+                            )
+                        })}
                     </div>
-                </div>
                 ) : null}
+
 
             </div>
 
             <div className='bottom'>
                 <div className='bottom-item recent-entry'>
                     <img src={assets.question_icon} alt='' />
-                   {extended? (<p>Help</p>) : null} 
+                    {extended ? (<p>Help</p>) : null}
 
                 </div>
 
                 <div className='bottom-item recent-entry'>
                     <img src={assets.history_icon} alt='' />
-                    {extended?(<p>Activity</p>):null }
+                    {extended ? (<p>Activity</p>) : null}
 
                 </div>
 
                 <div className='bottom-item recent-entry'>
                     <img src={assets.setting_icon} alt='' />
-                    {extended?(<p>Setting</p>):null }
+                    {extended ? (<p>Setting</p>) : null}
 
                 </div>
             </div>
--- src/context/Context.js
+++ src/context/Context.js
@@ -19,17 +19,30 @@ const ContextProvider = (props) => {
         },75*index)
     }
 
+    const newChat = () => {
+        setLoading(false);
+        setShowResult(false);
+    }
 
-    const onSent = async () => {
+
+    const onSent = async (prompt) => {
         setResultData('');
         setLoading(true);
         setShowResult(true);
-        setRecentPrompt(input);
-        setPrevPrompt(prev => [...prev,input]);
-        const response = await runChat(input);
+        
+        let response;
+        if(prompt !== undefined) {
+            response = await runChat(prompt);
+            setRecentPrompt(prompt);
+        }else{
+            setPrevPrompt(prev => [...prev,input]);
+            setRecentPrompt(input);
+            response = await runChat(input);
+        }
+        
         //to make the heading bold
         let responseArray = response.split("**");
-        let newResponse;
+        let newResponse="";
         for(let i=0;i<responseArray.length;i++){
             if(i === 0 || i%2 !== 1){
                 newResponse += responseArray[i];
@@ -62,7 +75,8 @@ const ContextProvider = (props) => {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
     }
 
     return (
